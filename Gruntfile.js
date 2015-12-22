a08bdd3144ce3d1d18bae58378833bf5e55ac7a2
zechau <zechau.h@gmail.com>
2015-12-22 18:59:40 +0800

    update grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,10 +13,8 @@ module.exports = function(grunt) {
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task(s).
   grunt.registerTask('default', ['uglify']);
 
 };
